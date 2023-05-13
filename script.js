65257b51d49e668719811578ae150866d20491ae
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-05-13 20:40:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,52 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
+//1st task
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(student){
+    if (student.marks>50){
+      console.log(student);
+    }
+  });
 }
 
+//2nd task
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
-}
+    arr.forEach(function(student) {
+        if (student.marks > 50) {
+          console.log(student);
+        }
+      });
+  }
+  PrintStudentsbyForEach();
 
+//3rd task
 function addData() {
   //Write your code here, just console.log
+  let newstudent={id:4, name:"susan", age:"20" , marks:45};
+  arr.push(newstudent);
+  console.log(arr);
 }
 
+//4th task
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(student => student.marks >= 50);
+  console.log(arr);
 }
+removeFailedStudent();
 
+//5th task
 function concatenateArray() {
   //Write your code here, just console.log
+  let secondArr=[
+    {id:6, name:"Kalvin", age:"20", marks:60},
+    {id:7, name:"James", age:"21", marks:47},
+    {id:8, name:"Rocky", age:"19", marks:76},
+  ]
+
+  arr=arr.concat(secondArr);
+  console.log(arr);
 }
